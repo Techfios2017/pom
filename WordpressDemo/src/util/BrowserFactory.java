c11b9c5376acd87c97f53881dfcb7e3bf0236d4d
Techfios2017 <30378494+Techfios2017@users.noreply.github.com>
2017-08-20 09:59:05 -0400

    modified browser factory

--- WordpressDemo/src/util/BrowserFactory.java
+++ WordpressDemo/src/util/BrowserFactory.java
@@ -7,27 +7,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserFactory {
 	
-	static WebDriver driver;
+static WebDriver driver;
 	
 	public static WebDriver startBrowser(String browserName, String url){
+		if(browserName.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", "../WordpressDemo/drivers/geckodriver.exe");
+			
+			ProfilesIni profile = new ProfilesIni();
+			FirefoxProfile myProfile = profile.getProfile("default");
+			DesiredCapabilities dc = DesiredCapabilities.firefox();
+			dc.setCapability(FirefoxDriver.PROFILE, myProfile);
+			dc.setCapability("marionette", true);
+			
+			driver = new FirefoxDriver(dc);
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
 		
+		else if(browserName.equalsIgnoreCase("IE"))
+		{
+			System.setProperty("webdriver.ie.driver", "../WordpressDemo/drivers/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
 		
-		 if(browserName.equalsIgnoreCase("chrome"))
+	
+		else if(browserName.equalsIgnoreCase("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver", "../WordpressDemo/drivers/chromedriver.exe");
-			
 			driver = new ChromeDriver();
 		}
 		
-		driver.get(url);
-		
+		driver.get(url);		
 		return driver;
 	}
-	
 	
 
 }
